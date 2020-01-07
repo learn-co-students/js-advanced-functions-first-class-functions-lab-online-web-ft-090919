7a304d75503cf978a44c7f901ec45787c296332f
Doug Kintop <dkintop@hotmail.com>
2020-01-07 12:19:21 -0500

    Done.

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ const returnLastTwoDrivers = function(drivers) {
   return [drivers[length - 1], drivers[length - 2]].reverse();
 };
 
-const selectingDrivers = function() {
-  return [returnFirstTwoDrivers, returnLastTwoDrivers];
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(integer) {
+  return function(fare) {
+    return fare * integer;
+  };
+}
+
+const fareDoubler = function(fare) {
+  return createFareMultiplier(2)(fare);
 };
+
+const fareTripler = function(fare) {
+  return createFareMultiplier(3)(fare);
+};
+
+function selectDifferentDrivers(arrayOfDdrivers, chosenFunction) {
+  return chosenFunction(arrayOfDdrivers);
+}
