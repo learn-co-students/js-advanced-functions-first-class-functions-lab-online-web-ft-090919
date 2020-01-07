d0644d4fb633c8fad180f6bdfac11b0c33199d28
Doug Kintop <dkintop@hotmail.com>
2020-01-07 11:41:59 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+  const innerFunction = function(drivers) {
+    return [drivers[0], drivers[1]];
+  };
+  return innerFunction;
+}
+console.log(returnFirstTwoDrivers(["first", "second", "third"]));
